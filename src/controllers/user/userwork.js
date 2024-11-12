b4822faa8901cfcae67a3a811f15c1eb389825df
Rishabh <promatics.vrishabh@gmail.com>
2024-11-12 11:47:17 +0530

    app api test

--- src/controllers/user/userwork.js
+++ src/controllers/user/userwork.js
@@ -6,9 +6,97 @@ const emailer = require("../../utils/emailer");
 const mongoose = require("mongoose");
 const generatePassword = require("generate-password");
 
+//create password for users
+function createNewPassword() {
+    const password = generatePassword.generate({
+        length: 8,
+        numbers: true,
+        uppercase: true,
+        lowercase: true,
+        strict: true,
+    });
+    return password;
+}
+
 exports.createBuyerProfile = async (req, res) => {
     try {
+        const data = req.body;
+        console.log("user data is ", data);
 
+        const doesEmailExists = await emailer.emailExists(data.email);
+        if (doesEmailExists)
+            return utils.handleError(res, {
+                message: "This email address is already registered",
+                code: 400,
+            });
+
+        if (data.phone_number) {
+            const doesPhoneNumberExist = await emailer.checkMobileExists(
+                data.phone_number
+            );
+            if (doesPhoneNumberExist)
+                return utils.handleError(res, {
+                    message: "This phone number is already registered",
+                    code: 400,
+                });
+        }
+        const password = createNewPassword();
+        const userData = {
+            ...data,
+            password,
+            decoded_password: password,
+            user_type: "buyer",
+            profile_completed: true,
+            is_user_approved_by_admin: true,
+        };
+
+        const user = new User(userData);
+        await user.save();
+
+        res.status(200).json({ message: "Buyer added successfully", data: user, code: 200 });
+
+    } catch (err) {
+        utils.handleError(res, err);
+    }
+}
+
+exports.getBuyerDetails = async (req, res) => {
+    try {
+        const user_id = req.params.id;
+        const user = await User.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(user_id),
+                },
+            },
+            {
+                $lookup: {
+                    from: "addresses",
+                    let: { user_id: "$_id" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: { $eq: ["$user_id", "$$user_id"] },
+                                is_primary: true,
+                            },
+                        },
+                    ],
+                    as: "address",
+                },
+            },
+            {
+                $unwind: {
+                    path: "$address",
+                    preserveNullAndEmptyArrays: true,
+                },
+            },
+            {
+                $addFields: {
+                    address: "$address.address",
+                },
+            },
+        ]);
+        res.json({ data: user[0], code: 200 });
     } catch (err) {
         utils.handleError(res, err);
     }
---
+++ src/routes/user/userwork.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+require("../../config/passport");
+const passport = require("passport");
+const requireAuth = passport.authenticate("jwt", {
+    session: false,
+});
+
+const controller = require("../../controllers/user/userwork");
+const trimRequest = require("trim-request");
+
+const validation = require("../../middleware/adminValidation");
+const validator = require("../../middleware/validator");
+
+//buyer routes
+
+//add buyer route
+router.post(
+    "/addNewBuyer",
+    trimRequest.all,
+    requireAuth,
+    controller.createBuyerProfile
+);
+
+router.get(
+    "/buyerdetails/:id",
+    trimRequest.all,
+    requireAuth,
+    controller.getBuyerDetails
+)
+
+
+module.exports = router;
