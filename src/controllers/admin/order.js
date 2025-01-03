82d0b613d29448c0796e54bb6bd0a7e668becee2
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-01-03 17:46:45 +0530

    changes

--- src/controllers/admin/order.js
+++ src/controllers/admin/order.js
@@ -39,7 +39,7 @@ exports.getOrders = async (req, res) => {
 exports.OrderDetails = async (req, res) => {
     try {
         const { id } = req.params
-        const order_data = await Order.findOne({ _id: id }).populate('order_items.product_id').populate('order_items.supplier_id').populate('order_items.logistics_id').populate('order_items.variant_id').populate('shipping_address').populate('billing_address').populate('payment_id').populate('tracking_id').populate('logistics_id')
+        const order_data = await Order.findOne({ _id: id }).populate('order_items.product_id').populate('order_items.supplier_id').populate('order_items.logistics_id').populate('order_items.variant_id').populate('shipping_address').populate('billing_address').populate('payment_id').populate('tracking_id').populate('logistics_id').populate('buyer_id')
         console.log("order_data : ", order_data)
 
         if (!order_data) {
