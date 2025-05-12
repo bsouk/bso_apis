8f13b355583c61b44daaa5dfdc79aad3e60b609d
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-05-12 18:11:25 +0530

    commited

--- src/controllers/admin/dashboard.js
+++ src/controllers/admin/dashboard.js
@@ -29,154 +29,6 @@ exports.dashboardChartData = async (req, res) => {
     }
 }
 
-// exports.getRevenueChartData = async (req, res) => {
-//     try {
-//         const { selectedPeriod } = req.query;
-//         let currentDate = new Date();
-//         let startOfPeriod, endOfPeriod;
-
-//         if (selectedPeriod === 'daily') {
-//             startOfPeriod = new Date(currentDate.setHours(0, 0, 0, 0));
-//             endOfPeriod = new Date(currentDate.setHours(23, 59, 59, 999));
-//         } else if (selectedPeriod === 'weekly') {
-//             const startOfWeek = new Date();
-//             startOfWeek.setDate(currentDate.getDate() - 6);
-//             startOfWeek.setHours(0, 0, 0, 0);
-
-//             const endOfWeek = new Date(startOfWeek);
-//             endOfWeek.setDate(startOfWeek.getDate() + 6);
-//             endOfWeek.setHours(23, 59, 59, 999);
-
-//             startOfPeriod = startOfWeek;
-//             endOfPeriod = endOfWeek;
-//         } else if (selectedPeriod === 'monthly') {
-//             const today = new Date();
-//             endOfPeriod = new Date(currentDate.setHours(0, 0, 0, 0));
-//             startOfPeriod = new Date(today.setMonth(today.getMonth() - 1));
-//         }
-//         else if (selectedPeriod === 'yearly') {
-//             const year = currentDate.getFullYear();
-//             const month = currentDate.getMonth()
-//             const date = currentDate.getDate()
-//             startOfPeriod = new Date(year - 1, month, date);
-//             // endOfPeriod = new Date(year, 11, 31, 23, 59, 59, 999);
-//             endOfPeriod = new Date(year, month, date);
-//         }
-
-//         console.log("start date:", startOfPeriod, "end date:", endOfPeriod);
-
-//         let filter = {
-//             user_type: 'personal'
-//         };
-//         let data = [];
-//         if (selectedPeriod) {
-//             filter.createdAt = { $gte: startOfPeriod, $lte: endOfPeriod };
-//         }
-
-//         console.log("filter:", filter);
-
-//         if (selectedPeriod === 'daily') {
-//             const dailyData = await Order.aggregate([
-//                 { $match: filter },
-//                 { $project: { hour: { $hour: "$createdAt" } } },
-//                 { $group: { _id: "$hour", count: { $sum: 1 } } },
-//                 { $sort: { _id: 1 } }
-//             ]);
-
-//             console.log("daily data:", dailyData);
-
-//             data = Array(24).fill(0);
-//             dailyData.forEach(item => {
-//                 data[item._id - 1] = item.count;
-//             });
-
-//         } else if (selectedPeriod === 'weekly') {
-//             const month = currentDate.getMonth();
-//             const year = currentDate.getFullYear();
-
-//             const startOfMonth = new Date(year, month, 1);
-//             const endOfMonth = new Date(year, month + 1, 0);
-//             console.log("startOfMonth : ", startOfMonth)
-//             console.log("endOfMonth : ", endOfMonth)
-//             // console.log("endOfMonth.getDate() + startOfMonth.getDay()) / 7 : ", (endOfMonth.getDate() + startOfMonth.getDay()) / 7)
-
-//             const weeksInMonth = Math.ceil((endOfMonth.getDate() + startOfMonth.getDay()) / 7);
-//             console.log("weeksInMonth : ", weeksInMonth)
-
-//             const weeklyData = await Order.aggregate([
-//                 { $match: filter },
-//                 {
-//                     $project: {
-//                         weekOfMonth: {
-//                             $ceil: {
-//                                 $divide: [
-//                                     { $subtract: ["$createdAt", startOfMonth] },
-//                                     1000 * 60 * 60 * 24 * 7
-//                                 ]
-//                             }
-//                         }
-//                     }
-//                 },
-//                 { $group: { _id: "$weekOfMonth", count: { $sum: 1 } } },
-//                 { $sort: { _id: 1 } }
-//             ]);
-
-//             console.log("weekly data:", weeklyData);
-
-//             data = Array(weeksInMonth).fill(0);
-//             weeklyData.forEach(item => {
-//                 data[item._id - 1] = item.count;
-//             });
-
-//         } else if (selectedPeriod === "monthly") {
-//             const daysInMonth = new Date(
-//                 currentDate.getFullYear(),
-//                 currentDate.getMonth() + 1,
-//                 0
-//             ).getDate();
-//             console.log("daysInMonth : ", daysInMonth)
-
-//             const monthlyData = await Order.aggregate([
-//                 { $match: filter },
-//                 { $project: { day: { $dayOfMonth: "$createdAt" } } },
-//                 { $group: { _id: "$day", count: { $sum: 1 } } },
-//                 { $sort: { _id: 1 } }
-//             ]);
-
-//             console.log("Monthly data:", monthlyData);
-
-//             data = Array(daysInMonth).fill(0);
-//             monthlyData.forEach(item => {
-//                 data[item._id - 1] = item.count;
-//             });
-//         }
-
-//         else if (selectedPeriod === 'yearly') {
-//             const yearlyData = await Order.aggregate([
-//                 { $match: filter },
-//                 { $project: { month: { $month: "$createdAt" } } },
-//                 { $group: { _id: "$month", count: { $sum: 1 } } },
-//                 { $sort: { _id: 1 } }
-//             ]);
-
-//             console.log("yearly data:", yearlyData);
-//             data = Array(12).fill(0);
-//             yearlyData.forEach(item => {
-//                 data[item._id - 1] = item.count;
-//             });
-//         }
-
-//         return res.json({
-//             message: "User overview fetched successfully",
-//             data,
-//             code: 200
-//         });
-
-//     } catch (error) {
-//         utils.handleError(res, error);
-//     }
-// }
-
 exports.getRevenueChartData = async (req, res) => {
     try {
         const { selectedPeriod } = req.query;
@@ -189,7 +41,6 @@ exports.getRevenueChartData = async (req, res) => {
         let currentDate = new Date();
         let startOfPeriod, endOfPeriod;
 
-        // Calculate date ranges based on selected period
         if (selectedPeriod === 'daily') {
             startOfPeriod = new Date(currentDate.setHours(0, 0, 0, 0));
             endOfPeriod = new Date(currentDate.setHours(23, 59, 59, 999));
@@ -226,35 +77,6 @@ exports.getRevenueChartData = async (req, res) => {
         let data = [];
 
         if (selectedPeriod === 'daily') {
-            // const dailyData = await Order.aggregate([
-            //     { $match: filter },
-            //     {
-            //         $project: {
-            //             hour: { $hour: "$createdAt" },
-            //             total_amount: 1
-            //         }
-            //     },
-            //     {
-            //         $group: {
-            //             _id: "$hour",
-            //             total: { $sum: "$total_amount" }
-            //         }
-            //     },
-            //     { $sort: { _id: 1 } }
-            // ]);
-            // console.log("dailyData :", dailyData);
-
-            // data = Array({ length: 24 }, (_, i) => ({
-            //     hour: i,
-            //     total: 0
-            // }));
-            // dailyData.forEach(item => {
-            //     data[item._id] = {
-            //         hour: item._id,
-            //         total: parseFloat(item.total.toFixed(2))
-            //     }
-            // });
-
             const dailyData = await Order.aggregate([
                 { $match: filter },
                 {
@@ -288,46 +110,6 @@ exports.getRevenueChartData = async (req, res) => {
             });
 
         } else if (selectedPeriod === 'weekly') {
-            // const month = currentDate.getMonth();
-            // const year = currentDate.getFullYear();
-
-            // const startOfMonth = new Date(year, month, 1);
-            // const endOfMonth = new Date(year, month + 1, 0);
-            // console.log("startOfMonth : ", startOfMonth)
-            // console.log("endOfMonth : ", endOfMonth)
-            // // console.log("endOfMonth.getDate() + startOfMonth.getDay()) / 7 : ", (endOfMonth.getDate() + startOfMonth.getDay()) / 7)
-
-            // const weeksInMonth = Math.ceil((endOfMonth.getDate() + startOfMonth.getDay()) / 7);
-            // console.log("weeksInMonth : ", weeksInMonth)
-            // const weeksData = await Order.aggregate([
-            //     { $match: filter },
-            //     {
-            //         $project: {
-            //             weekOfMonth: {
-            //                 $ceil: {
-            //                     $divide: [
-            //                         { $subtract: ["$createdAt", startOfMonth] },
-            //                         1000 * 60 * 60 * 24 * 7
-            //                     ]
-            //                 }
-            //             }
-            //         }
-            //     },
-            //     { $group: { _id: "$weekOfMonth", total: { $sum: "$total_amount" } } },
-            //     { $sort: { _id: 1 } }
-            // ]);
-            // console.log("weeksData :", weeksData);
-
-            // data = Array({ length: weeksInMonth }, (_, i) => ({
-            //     week: i,
-            //     total: 0
-            // }));
-            // weeksData.forEach(item => {
-            //     data[item._id - 1] = {
-            //         week: item._id,
-            //         total: item.total
-            //     }
-            // });
 
             const currentDate = new Date();
             const month = currentDate.getMonth();
