a4cfaa45d9b5cf9ef6669b3b8d00e743de74d1fb
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-05-13 10:38:50 +0530

    job schema changed

--- src/models/jobs.js
+++ src/models/jobs.js
@@ -5,6 +5,9 @@ const jobSchema = new mongoose.Schema({
     job_unique_id: {
         type: String
     },
+    company_description: String,
+    phone_number: Number,
+    email: String,
     job_title: {
         type: String
     },
@@ -21,10 +24,57 @@ const jobSchema = new mongoose.Schema({
         ref: 'users'
     },
     job_type: {
-        type: String,
-        enum: ["Full-Time", "Part-Time", "Freelance", "Contract-Based", "Internship", "Temporary", "Remote", "On-Site", "Hybrid"],
-        default: 'full-time'
+        type: [String],
+        enum: ["Full-Time", "Part-Time", "Freelance", "Contract-Based", "Internship", "Temporary", "Hybrid"],
     },
+    schedule: {
+        type: [String],
+        enum: ["Day-shift", "Morning-shift", "Rotational-shift", "Night-shift", "Monday-Friday", "Evening-shift", "Weekend-availabilty", "Fixed-shift", "US-shift", "UK-shift", "Weekend-only", "other", "none"]
+    },
+    rate: {
+        type: String,
+        enum: ["Hourly", "Monthly", "Annually", "Per-project", "Weekly", "Daily", "Other"]
+    },
+    supplemental_pay: {
+        type: [String],
+    },
+    benefit: [String],
+    job_location_type: {
+        type: String,
+        enum: ["On-Site", "Remote"]
+    },
+    advertise: {
+        status: {
+            type: String,
+            enum: ["yes", "no"],
+        },
+        city: String
+    },
+    office_address: {
+        address_line_1: String,
+        address_line_2: String,
+        city: String,
+        state: String,
+        country: String,
+        pincode: String,
+        area: String
+    },
+    planned_start_date: {
+        status: {
+            type: String,
+            enum: ["yes", "no"]
+        },
+        date: Date
+    },
+    application_last_date: {
+        status: {
+            type: String,
+            enum: ["yes", "no"]
+        },
+        date: Date
+    },
+    vacancy: String,
+    recruitment_timeline: String,
     location: {
         type: String
     },
@@ -43,8 +93,15 @@ const jobSchema = new mongoose.Schema({
     },
     experience: Number,
     skills: [String],
-    budget: String,
-    payment_type: String,
+    payment_type: {
+        type: {
+            type: String,
+            enum: ['Range', 'Minimum', 'Maximum', 'Exact']
+        },
+        value: Number,
+        min: Number,
+        max: Number
+    },
     job_duration: String,
     weekly_hour_requirement: String,
     project_visibility: String,
@@ -79,6 +136,14 @@ const jobSchema = new mongoose.Schema({
         type: String,
         enum: ['active', 'inactive', 'expired'],
         default: 'inactive'
+    },
+    get_individual_email: {
+        type: Boolean,
+        default: false
+    },
+    let_candidate_contact_us: {
+        type: Boolean,
+        default: false
     }
 },
     { timestamps: true }
