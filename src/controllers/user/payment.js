fa4da3eb2fa6264e14cee08217c284d3d0a4a67f
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-05-14 11:54:03 +0530

    done!

--- src/controllers/user/payment.js
+++ src/controllers/user/payment.js
@@ -266,7 +266,7 @@ exports.createPaymentIntent = async (req, res) => {
             paymentAmount = enquiry_data?.grand_total
         }
 
-        if (fetch_term.method == "scheduled" && fetch_term.schedule && fetch_term.schedule.length > 0 && paymenthistory.payment_stage && paymenthistory.payment_stage.length > 0) {
+        if (fetch_term.method == "scheduled" && fetch_term.schedule && fetch_term.schedule.length > 0) {
             for (const i of fetch_term.schedule) {
                 if (!paymenthistory.payment_stage.includes(i.schedule_id)) {
                     paymentAmount = i.value_type === "percentage"
