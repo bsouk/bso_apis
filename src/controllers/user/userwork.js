553b2f028ed084f45c48a02e8d66b207c3138925
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-05-27 16:01:32 +0530

    done!

--- src/controllers/user/userwork.js
+++ src/controllers/user/userwork.js
@@ -3620,7 +3620,7 @@ exports.AddTeamMember = async (req, res) => {
         if (teamdata?.members?.length >= 3) {
             const Member = await UserMember.findOne({ user_id: teamdata.admin_id });
             console.log("Member : ", Member)
-            if (teamdata.members.length >= Member.member_count) {
+            if (teamdata?.members?.length >= Member?.member_count) {
                 return res.status(402).json({
                     message: "You have reached your member limit",
                     code: 402
