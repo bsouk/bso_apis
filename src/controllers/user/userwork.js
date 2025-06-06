188e4bf32aba48ffd794d9990e92dd7c0c4a5278
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-06-06 11:18:57 +0530

    commited

--- src/controllers/user/userwork.js
+++ src/controllers/user/userwork.js
@@ -5783,7 +5783,7 @@ exports.getSingleSupplierQuotes = async (req, res) => {
             .populate('payment_terms')
             .populate('admin_payment_terms')
             .populate("pickup_address")
-            .populate({ path: 'enquiry_id', select: 'priority shipping_address enquiry_unique_id expiry_date', populate: { path: 'shipping_address' } })
+            .populate({ path: 'enquiry_id', select: 'priority shipping_address enquiry_unique_id expiry_date delivery_selection_data', populate: { path: 'shipping_address' } })
             .sort({ createdAt: -1 })
         return res.status(200).json({
             message: "Supplier quote data fetched successfully",
