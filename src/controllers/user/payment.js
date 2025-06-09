6a1813bd6c9b994484699d74dfbf2fb85f59611f
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-06-09 15:19:57 +0530

    commited

--- src/controllers/user/payment.js
+++ src/controllers/user/payment.js
@@ -381,7 +381,7 @@ exports.createPaymentIntentlogisticsupplier = async (req, res) => {
             customer = await createStripeCustomer(user);
         }
 
-        let paymenthistory = await Payment.findOne({ enquiry_id: enquiry_id, supplier_id: userId });
+        let paymenthistory = await Payment.findOne({ enquiry_id: enquiry_id, buyer_id: enquiry_data?.user_id });
         console.log("paymenthistory : ", paymenthistory)
 
         let totalAmount = enquiry_data?.grand_total
@@ -390,7 +390,7 @@ exports.createPaymentIntentlogisticsupplier = async (req, res) => {
         if (!paymenthistory) {
             paymenthistory = await Payment.create({
                 enquiry_id: enquiry_id,
-                supplier_id: userId,
+                buyer_id: enquiry_data?.user_id,
                 total_amount: totalAmount,
                 payment_status: 'pending',
                 stripe_customer_id: customer.id,
