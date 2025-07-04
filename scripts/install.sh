aeeeffcef1a4b898a4de80da644ac0ce914be90b
bsouk <bsouk.ltd@gmail.com>
2025-07-04 16:48:05 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,13 +5,10 @@ set -e
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Use specific Node.js versionsss
-#nvm use v20 || { echo "Error: Failed to load Node.js v20"; exit 1; }
-#echo "NVM Loaded"
 echo "Node Version: $(node -v)"
 echo "Running as: $(whoami)"
 echo "Changing Directory..."
-cd /home/bsoservices/ci-cd/api || { echo "Error: Directory not found"; exit 1; }
+cd /var/www/mongo/bso_apis || { echo "Error: Directory not found"; exit 1; }
 echo "Current Directory: $(pwd)"
 echo "Installing NPM Packages..."
 npm i || { echo "Error: NPM installation failed"; exit 1; }
