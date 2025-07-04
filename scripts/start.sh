70a76045c6f52cb1a5c9013f47a9e211d7da3b2a
bsouk <bsouk.ltd@gmail.com>
2025-07-04 17:26:42 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Fail script if any command fails
-set -e
 # Load NVM (Securely)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -8,10 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 echo "Node Version: $(node -v)"
 echo "Running as: $(whoami)"
 echo "Changing Directory..."
-cd /var/www/mongo/bso_apis || { echo "Error: Directory not found"; exit 1; }
+cd /var/www/mongo/bso_apis
 echo "Current Directory: $(pwd)"
-echo "Checking PM2 Processes"
-pm2 status
 echo "Starting APIs"
 pm2 restart server.js
-pm2 status
